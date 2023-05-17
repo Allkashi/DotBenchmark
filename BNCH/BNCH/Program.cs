f2c326a5a1f80280ce6a9a36aa7d68ba031eca3e
Allkashi <mail50025@gmail.com>
2023-05-17 16:28:43 +0500

    Ver1.0
    This reverts commit e8c55b9ed8dac1d48489979307b8a58c98e9a316.

---
+++ BNCH/BNCH/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace benchmark
+{
+
+
+    public class SortsBenchmark
+    {
+        private SortA sortA = new SortA();
+        private const int ArraySize = 10000;
+        private int[] Array;
+
+        public SortsBenchmark()
+        {
+            Random rand = new Random();
+            Array = new int[ArraySize];
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                Array[i] = rand.Next();
+            }
+        }
+
+        [Benchmark]
+        public void BubbleSort()
+        {
+            sortA.BubbleSort(Array);
+        }
+
+        [Benchmark]
+        public void QuickSort()
+        {
+            sortA.QuickSort(Array, 0, ArraySize - 1);
+        }
+
+        [Benchmark]
+        public void InsertionSort()
+        {
+            sortA.InsertionSort(Array);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SortsBenchmark>();
+        }
+    }
+}
+
---
+++ BNCH/BNCH/SortA.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace benchmark
+{
+    class SortA
+    {
+        public void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // Быстрая сортировка
+        public void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        private int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int temp2 = arr[i + 1];
+            arr[i + 1] = arr[right];
+            arr[right] = temp2;
+            return i + 1;
+        }
+
+        // Сортировка вставками
+        public void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
